73b6a7268fc75ae09f90270f8d54ddd22084b778
Eldstrale <calle.eld@hotmail.com>
2020-02-17 10:08:28 +0100

    css

--- lektion.js
+++ lektion.js
@@ -5,6 +5,12 @@ const port = 3000
 var clientDir = __dirname + '\\client\\'
 
 app.get('/', (req, res) => {
-    res.sendFile(clientDir + 'lektion.html')
+    res.sendFile(__dirname + 'lektion.html')
 })
+
+app.get('/style.css', (reg,res) => {
+
+    res.sendFile(__dirname + 'style.css')
+})
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
